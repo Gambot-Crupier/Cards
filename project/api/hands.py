fbf35c63f6fa2ebc4a1f00e13154b37a71945329
Bernardo Henrique <bernardohrl.unb@gmail.com>
2019-10-04 17:48:47 -0300

    Refactors post_hands
    Also start get_hands

--- project/api/hands.py
+++ project/api/hands.py
@@ -9,30 +9,24 @@ import json, sys
 hands_blueprint = Blueprint('card', __name__)
 
 
-@hands_blueprint.route('/get_fake_hands', methods=['GET'])
-def get_fake_hand():
-
-    UPLOADS_PATH = join(dirname(realpath(__file__)), 'fake_data/hands.json')
-
-    with open(UPLOADS_PATH, 'r') as f:
-        hands_json = json.load(f)
-
-    save_hands(hands_json)
-
-    return jsonify({
-        'hand': [
-            { 'Sucess': 'full' }
-        ] 
-    })
-
-
 @hands_blueprint.route("/post_hands", methods=["POST"])
 def post_hands():
     try:
         hands_json = request.get_json()
-        print(hands_json, file=sys.stderr)
     
-        save_hands(hands_json)
+        for hand in hands_json:
+            player_id = hand['player_id']
+            round_id = hand['round_id']
+            cards = hand['cards']
+            
+            for card in cards:
+                value = card['value']
+                suit = card['suit']
+
+                card = Card.query.filter_by(value=value, suit=suit).first()
+                db.session.add(Hand(player_id=player_id, card_id=card.id, round_id=round_id))
+
+        db.session.commit()
     
     except HTTPError:
         return jsonify({"message": "NOT FOUND", "status_code": 404}), 404
@@ -40,16 +34,48 @@ def post_hands():
         return jsonify({"message": "Hands Recived", "status_code": 200}), 200
 
 
-def save_hands(hands_json):
-    for hand in hands_json:
-        player_id = hand['player_id']
-        db.session.add(Hand(player_id=player_id))
-        
-        cards = hand['cards']
-        for card in cards:
-            value = card['value']
-            suit = card['suit']
-            db.session.add(Card(player_id=player_id, value=value, suit=suit))
+@hands_blueprint.route("/get_hands", methods=["GET"])
+def get_hands():
+    try:
+        round_id = request.args.get('round_id')
+        round_hands = Hand.query.filter_by(round_id = round_id).all()
 
-        db.session.commit()
+        response = {
+            "round_id": round_id,
+            "hands": []
+        }
+
+        player_ids_set = set([]) 
+        for hand in round_hands:
+            player_ids_set.add(hand.player_id)
+
+        for player_id in list(player_ids_set):
+            player_hands = Hand.query.filter_by(round_id = round_id, player_id=player_id).all()
+
+            hand = {
+                "player_id": player_id,
+                "cards": []
+            }
+
+            for hand in player_hands:
+                card = Card.query.filter_by(id=hand.card_id).first()
+                
+                hand['cards'].append({
+                    "value": card.value,
+                    "suit": card.suit
+                })
+
+            response['hands'].append(hand)
+
+        print(response, file=sys.stderr)
+
+
+        
     
+
+        # db.session.commit()
+    
+    except HTTPError:
+        return jsonify({"message": "NOT FOUND", "status_code": 404}), 404
+    else:
+        return jsonify({"message": "hands", "status_code": 200}), 200
