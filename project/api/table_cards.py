67e029cfffaa249531c47770abf8a4eb9c368f7d
Bruno Dantas <oliveiradantas96@gmail.com>
2019-10-04 11:37:14 -0300

    Adicionando método para pegar e salvar cartas compartilhadas da partida.

--- project/api/table_cards.py
+++ project/api/table_cards.py
@@ -15,35 +15,27 @@ def post_table_cards():
     try:
         table_cards_json = request.get_json()
         
-        # save_table_cards(table_cards_json)
+        save_table_cards(table_cards_json)
     
     except HTTPError:
         return jsonify({"message": "NOT FOUND", "status_code": 404}), 404
     else:
         return jsonify({"message": "Table Cards Recived", "status_code": 200}), 200
      
-# def save_table_cards(table_cards_json):
-#     for card in table_cards_json:
-#         round_id = card['round_id']
-#         print(round_id, file=sys.stderr)
-#         db.session.add(RoundCards(round_id))
+def save_table_cards(table_cards_json):
+    for card in table_cards_json:
+        round_id = card['round_id']
         
-#     cards = card['cards']     
-#     for table_card in cards:
-#         value = table_card['value']
-#         suit = table_card['suit']
+        cards = card['cards']  
         
-#         card_value = Card.query.filter_by(value='value').first()
-#         card_suit = Card.query.filter_by(suit='suit').first()
+        for table_card in cards:
+            value = table_card['value']
+            suit = table_card['suit']
 
-#         if value and suit is card_value and card_suit:
-#             card_id = Card.query.filter_by(id='id').first()
-        
-#         else:
-#             return jsonify({"message": "Not match table cards."})      
-        
-#         db.session.add(RoundCards(card_id=card_id))
+            card = Card.query.filter_by(value=value, suit=suit).first()
 
-#     db.session.commit()
+            db.session.add(RoundCards(round_id=round_id, card_id=card.id))
+            
+    db.session.commit()
         
     
